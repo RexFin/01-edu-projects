616949bed14504ab87e84fb39c852e7102c0a8e3
Nurik <kishibaevnurzhan@gmail.com>
2026-05-07 22:29:41 +0500

    Added JS functional and go server

--- golang-projects/ascii-art-web/main.go
+++ golang-projects/ascii-art-web/main.go
@@ -1,5 +1,40 @@
 package main
 
-func main() {
+import (
+	"fmt"
+	"log"
+	"net/http"
+)
 
+func formHandler(w http.ResponseWriter, r *http.Request) {
+	err := r.ParseForm()
+	if err != nil {
+		fmt.Fprintf(w, "ParseForm() err: %v\n", err)
+		return
+	}
+
+	fmt.Fprintf(w, "POST Request successful\n")
+	text := r.FormValue("text")
+	bannerType := r.FormValue("banner-type")
+
+	fmt.Fprintf(w, "Text = %s\n", text)
+	fmt.Fprintf(w, "BannerType = %s\n", bannerType)
+
+	fmt.Println(text)
+	fmt.Println(bannerType)
+
+}
+
+func main() {
+	http.HandleFunc("/ascii-art", formHandler)
+
+	fileserver := http.FileServer(http.Dir("./template"))
+	http.Handle("/", fileserver)
+
+	fmt.Printf("Starting server at port 8181\n")
+
+	serverError := http.ListenAndServe(":8181", nil)
+	if serverError != nil {
+		log.Fatal(serverError)
+	}
 }
