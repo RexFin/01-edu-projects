09d339361d48425041d4a59ada505172e3d40f28
Nurik <83882346+RexFin@users.noreply.github.com>
2026-05-27 19:42:29 +0500

    New refactored main.go file

--- golang-projects/ascii-art-web/main.go
+++ golang-projects/ascii-art-web/main.go
@@ -1,40 +1,15 @@
 package main
 
 import (
+	"ascii-art-web/handlers"
 	"fmt"
-	"log"
 	"net/http"
 )
 
-func formHandler(w http.ResponseWriter, r *http.Request) {
-	err := r.ParseForm()
-	if err != nil {
-		fmt.Fprintf(w, "ParseForm() err: %v\n", err)
-		return
-	}
-
-	fmt.Fprintf(w, "POST Request successful\n")
-	text := r.FormValue("text")
-	bannerType := r.FormValue("banner-type")
-
-	fmt.Fprintf(w, "Text = %s\n", text)
-	fmt.Fprintf(w, "BannerType = %s\n", bannerType)
-
-	fmt.Println(text)
-	fmt.Println(bannerType)
-
-}
-
 func main() {
-	http.HandleFunc("/ascii-art", formHandler)
+	http.HandleFunc("/", handlers.HomeHandler)
+	http.HandleFunc("/ascii-art", handlers.AsciiHandler)
 
-	fileserver := http.FileServer(http.Dir("./template"))
-	http.Handle("/", fileserver)
-
-	fmt.Printf("Starting server at port 8181\n")
-
-	serverError := http.ListenAndServe(":8181", nil)
-	if serverError != nil {
-		log.Fatal(serverError)
-	}
+	fmt.Println("Server started at http://localhost:8080")
+	http.ListenAndServe(":8080", nil) // Maybe create own router...?
 }
