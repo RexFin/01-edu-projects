318ab3d3bd7a9a978c1d61bf61b9eee6fb53c753
Nurik <83882346+RexFin@users.noreply.github.com>
2026-06-07 21:03:11 +0500

    static file system added

--- golang-projects/ascii-art-web/main.go
+++ golang-projects/ascii-art-web/main.go
@@ -10,6 +10,9 @@ func main() {
 	http.HandleFunc("/", handlers.HomeHandler)
 	http.HandleFunc("/ascii-art", handlers.AsciiHandler)
 
+	fs := http.FileServer(http.Dir("./static"))
+	http.Handle("/static/", http.StripPrefix("/static/", fs))
+
 	fmt.Println("Server started at http://localhost:8080")
 	http.ListenAndServe(":8080", nil) // Maybe create own router...?
 }
