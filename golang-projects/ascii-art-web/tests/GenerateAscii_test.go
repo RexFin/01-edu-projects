6f98f4797ef4bae1ac7a9f48a765d5300f0e16d4
Nurik <83882346+RexFin@users.noreply.github.com>
2026-06-07 21:10:37 +0500

    Unit tests added

---
+++ golang-projects/ascii-art-web/tests/GenerateAscii_test.go
@@ -0,0 +1,36 @@
+package tests
+
+import (
+	"ascii-art-web/pkg"
+	"fmt"
+	"os"
+	"testing"
+)
+
+func TestGenerateAscii(t *testing.T) {
+	originalDir, _ := os.Getwd()
+	os.Chdir("..")
+	defer os.Chdir(originalDir)
+
+	var tests = []struct {
+		name, text, banner string
+		want               string
+	}{
+		{"Empty test", "", "", ""},
+		{"Hello test", "Hello", "standard", " _    _          _   _          \n| |  | |        | | | |         \n| |__| |   ___  | | | |   ___   \n|  __  |  / _ \\ | | | |  / _ \\  \n| |  | | |  __/ | | | | | (_) | \n|_|  |_|  \\___| |_| |_|  \\___/  \n                                \n                                \n"},
+		{"Hello newline test", "Hello\n", "standard", " _    _          _   _          \n| |  | |        | | | |         \n| |__| |   ___  | | | |   ___   \n|  __  |  / _ \\ | | | |  / _ \\  \n| |  | | |  __/ | | | | | (_) | \n|_|  |_|  \\___| |_| |_|  \\___/  \n                                \n                                \n\n"},
+		{"Hello newline There test", "Hello\nThere", "standard", " _    _          _   _          \n| |  | |        | | | |         \n| |__| |   ___  | | | |   ___   \n|  __  |  / _ \\ | | | |  / _ \\  \n| |  | | |  __/ | | | | | (_) | \n|_|  |_|  \\___| |_| |_|  \\___/  \n                                \n                                \n _______   _                           \n|__   __| | |                          \n   | |    | |__     ___   _ __    ___  \n   | |    |  _ \\   / _ \\ | '__|  / _ \\ \n   | |    | | | | |  __/ | |    |  __/ \n   |_|    |_| |_|  \\___| |_|     \\___| \n                                       \n                                       \n"},
+		{"Hello newline newline There test", "Hello\n\nThere", "standard", " _    _          _   _          \n| |  | |        | | | |         \n| |__| |   ___  | | | |   ___   \n|  __  |  / _ \\ | | | |  / _ \\  \n| |  | | |  __/ | | | | | (_) | \n|_|  |_|  \\___| |_| |_|  \\___/  \n                                \n                                \n\n _______   _                           \n|__   __| | |                          \n   | |    | |__     ___   _ __    ___  \n   | |    |  _ \\   / _ \\ | '__|  / _ \\ \n   | |    | | | | |  __/ | |    |  __/ \n   |_|    |_| |_|  \\___| |_|     \\___| \n                                       \n                                       \n"},
+	}
+
+	for _, tt := range tests {
+		testname := fmt.Sprintf("%s,%s", tt.text, tt.banner)
+		t.Run(testname, func(t *testing.T) {
+			ans, err := pkg.GenerateAscii(tt.text, tt.banner)
+			if ans != tt.want {
+				fmt.Println("GenerateAscii error:", err)
+				t.Errorf("[%s] got \n%s, want \n%s", tt.name, ans, tt.want)
+			}
+		})
+	}
+}
